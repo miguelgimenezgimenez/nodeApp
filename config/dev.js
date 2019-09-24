01adceb0419b49c486fb24dbef3fc6628a7bb77a
Miguel Gimenez <miguel.gimenez@primaverasound.com>
2019-09-24 08:01:00 +0200

    add keys

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,3 @@
-// Don't be like me and commit this file!
-// These keys have been disabled, but remain here for reference purposes.
 module.exports = {  
   mongoURI: 'mongodb://127.0.0.1:27017/nodeServer',
   cookieKey: '123123123'
